335f3787ee538e14da5b083785bfc0f9421a5229
Matan P <matan.pazuelo@danhotels.com>
2019-01-15 21:53:13 +0200

    react router with server side rendring examples

---
+++ README.md
@@ -0,0 +1,5 @@
+# Matan Pazuelo
+
+part of react-router experimenting. react router server side rendering
+
+This project uses [Create React App](https://github.com/facebookincubator/create-react-app).
---
+++ src/shared/Navbar.js
@@ -0,0 +1,46 @@
+import React from 'react'
+import { NavLink } from 'react-router-dom'
+
+export default function Navbar() {
+  const languages = [
+    {
+      name: 'All',
+      param: 'all'
+    },
+    {
+      name: 'JavaScript',
+      param: 'javascript'
+    },
+    {
+      name: 'Ruby',
+      param: 'ruby'
+    },
+    {
+      name: 'Python',
+      param: 'python'
+    },
+    {
+      name: 'Java',
+      param: 'java'
+    },
+    {
+      name: 'PHP',
+      param: 'php'
+    }
+  ]
+
+  return (
+    <ul>
+      {languages.map(({ name, param }) => (
+        <li key={param}>
+          <NavLink
+            activeStyle={{ fontWeight: 'bold' }}
+            to={`/popular/${param}`}
+          >
+            {name}
+          </NavLink>
+        </li>
+      ))}
+    </ul>
+  )
+}
